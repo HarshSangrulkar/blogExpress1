8fb5f2bc62ff4c0f9c78a9835173249f33fe614a
Harsh Sangrulkar <harshsangrulkar@gmail.com>
2023-07-13 02:19:43 +0530

    adding files using git

---
+++ data/blogs.js
@@ -0,0 +1,21 @@
+
+blogs = [
+    {
+        title: "Learn HTML",
+        constent: "This is html content",
+        slug: "html-learn"
+    },
+    {
+        title: "Learn CSS",
+        constent: "This is css content",
+        slug: "css-learn"
+    },
+    {
+        title: "Learn Javascript",
+        constent: "This is js content",
+        slug: "js-learn"
+    },
+
+]
+
+module.exports = blogs;
---
+++ routes/blog.js
@@ -0,0 +1,32 @@
+const express = require('express')
+const path = require('path')
+const blogs = require('../data/blogs')
+//const e = require('express')
+
+const router = express.Router()
+
+
+router.get('/', (req,res)=>{
+    //res.sendFile(path.join(__dirname, '../templates/index.html'))
+    res.render('home');
+});
+router.get('/blog', (req,res)=>{
+    /*blogs.forEach(e => {
+        console.log(e.title)
+    });*/
+    //res.sendFile(path.join(__dirname, '../templates/bloghome.html'))
+    res.render('blogHome', {
+        blogs: blogs
+    });
+});
+
+router.get('/blogpost/:slug', (req,res)=>{
+    myBlog = blogs.filter((e)=>{
+        return e.slug == req.params.slug
+    })
+    console.log(myBlog)
+    res.sendFile(path.join(__dirname, '../templates/blogpage.html'))
+});
+
+
+module.exports = router
